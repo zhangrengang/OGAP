d337ba8d631421a5e368769c27a82491789d7dc0
zhangrengang <zhangrenang@ori-gene.cn>
2020-04-09 12:50:05 +0800

    readme

--- build.sh
+++
@@ -1,7 +0,0 @@
-tp=mt
-tx=rosids
-[ $1 ] && tp=$1
-[ $2 ] && tx=$2
-[ $tp = mt ] && gb=~/database/RefSeq/mitochondrion/mitochondrion.*.genomic.gbff.gz
-[ $tp = pt ] && gb=~/database/RefSeq/plastid/plastid.*.genomic.gbff.gz
-python /share/home/nature/users/zhangrenang/ogap/makedb.py -gbfiles $gb -organ $tp -taxon $tx 2>&1 | tee build.$tp-$tx.log
